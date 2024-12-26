5ac93bb6ae271bbcb386a4066100ea6af24f1ce8
Ankit Pandey <114949005+ankitpandey88@users.noreply.github.com>
2024-12-26 11:20:12 +0530

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
